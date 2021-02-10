6a5fae1d9cec5e79ca46139eaf70de32f5fdaf8d
Anurag Das <anurag.das23.ad@gmail.com>
2021-02-10 08:30:58 -0500

    Connected server to DB

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '',
+      database : 'smartbrain'
+    }
+  });
+
+console.log(postgres.select().table('users'));  
 
 const app = express();
 
